05eba8b9570a534cd41783f8147c16fec6dd061d
Mefi8427 <olena.wierchowicz@icloud.com>
2023-12-30 13:59:20 +0100

    ye
    summary

---
+++ global.gd
@@ -0,0 +1,42 @@
+extends Node2D
+var LeftPressed
+var RightPressed
+var RPressed
+var random = RandomNumberGenerator.new()
+var x 
+var y
+var game_over = 0
+@onready var Pomni = get_node("/root/Node2D/pomni")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	movement()
+	restart()
+	
+
+
+
+func movement():
+	if game_over == 1:
+		return
+	LeftPressed = int(Input.is_action_just_pressed("left"))
+	RightPressed = int(Input.is_action_just_pressed("right"))
+	
+	if LeftPressed == 1:
+		Pomni.position.x = 468
+	elif RightPressed == 1:
+		Pomni.position.x = 681
+		
+func restart():
+	RPressed = int(Input.is_action_just_pressed("restart"))
+	
+	if RPressed == 1:
+		get_tree().reload_current_scene()
+
+	
+		
---
+++ kamera.gd
@@ -0,0 +1,70 @@
+extends Sprite2D
+var random = RandomNumberGenerator.new()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	death()
+	movement()
+	going_back()
+	fix()
+	
+	
+	
+func movement():
+	if Global.game_over == 1:
+		return
+	elif Global.RightPressed == 1 or Global.LeftPressed == 1:
+		position.y += 124
+		
+func death():
+	if Global.Pomni.position.x == 468 and position == Vector2(482,621):
+		Global.game_over = 1
+	elif Global.Pomni.position.x == 681 and position == Vector2(657, 621):
+		Global.game_over = 1
+		
+func going_back():
+	if Global.game_over == 1:
+		return
+	var d2 = random.randi_range(1,2)
+	var d5 = random.randi_range(1,5)
+	if position.y >= 745:
+		if d2 == 1:
+			Global.x = 482
+		elif d2 == 2:
+			Global.x = 657
+		position.x = Global.x
+		
+		if d5 == 1 or d5 == 2:
+			Global.y = 125
+		elif d5 == 3 or d5 == 4:
+			Global.y = 1
+		else:
+			Global.y = -123
+		position.y = Global.y
+		
+func fix():
+	if Global.Pomni.position.x == 681 and position == Vector2(482,621) or Global.Pomni.position.x == 468 and position == Vector2(657, 621):
+		var d2 = random.randi_range(1,2)
+		var d5 = random.randi_range(1,5)
+		if d2 == 1:
+			Global.x = 482
+		elif d2 == 2:
+			Global.x = 657
+		position.x = Global.x
+			
+		if d5 == 1 or d5 == 2:
+			Global.y = 125
+		elif d5 == 3 or d5 == 4:
+			Global.y = 1
+		else:
+			Global.y = -123
+		position.y = Global.y
+		
+
+	
