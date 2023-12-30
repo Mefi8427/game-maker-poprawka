a77bb9cf2a16f3d9e012ed008552fbca11f06215
Mefi8427 <olena.wierchowicz@icloud.com>
2023-12-30 19:07:55 +0100

    d
    d

--- global.gd
+++ global.gd
@@ -8,7 +8,9 @@ var y
 var game_over = 0
 @onready var Pomni = get_node("/root/Node2D/pomni")
 var score = 0
-@onready var scoreLabel = get_node("root/Node2D/scoreLabel")
+@onready var scoreLabel = get_node("/root/Node2D/scoreLabel")
+@onready var BarItself = get_node("/root/Node2D/TextureProgressBar")
+var bar_negative = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,12 +22,33 @@ func _process(_delta):
 	movement()
 	restart()
 	player_score()
+	progress_bar()
 	
 	
 func player_score():
 	if game_over == 1:
 		return
 	scoreLabel.text = str(score)
+	if LeftPressed == 1 or RightPressed == 1:
+		score += 1
+		
+func progress_bar():
+	bar_negative = float(score * 0.005)
+	if bar_negative < 0.055:
+		bar_negative = 0.06
+	if score > 40:
+		bar_negative = 0.1
+	if score > 50:
+		bar_negative = 0.15
+	if score > 60:
+		bar_negative = 0.2
+	if score > 70:
+		bar_negative = 0.25
+	if score > 80:
+		bar_negative = 0.3
+	BarItself.value -= bar_negative
+	if BarItself.value == 0:
+		game_over = 1
 	
 	
 
